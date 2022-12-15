c2d4196e89a6b31d856b47824b5512a769ee1c48
Dominik "Dodo" Dopplinger <dominik.dopplinger@kununu.com>
2022-12-15 13:29:35 +0100

    Solution level 15, part 2

--- solutions/level15.py
+++ solutions/level15.py
@@ -1,3 +1,4 @@
+import time
 from enum import Enum
 from typing import List, Tuple
 
@@ -48,7 +49,7 @@ class Field:
             )
         self.width = self.max_x - self.min_x + 1
 
-    def calc_signal_spots_for_line(self, y: int) -> int:
+    def calc_signal_spots_for_line(self, y: int) -> List[Position]:
         line = [Position.Unknown] * self.width
 
         for sensor in self.sensors:
@@ -69,8 +70,31 @@ class Field:
                 x = sensor.beacon_x - self.min_x
                 line[x] = Position.Beacon
 
-        # print("".join(map(lambda p: p.value, line)))
-        return len(list(filter(lambda p: p == Position.Signal, line)))
+        return line
+
+    def get_uncovered_x(self, y: int, limit_x: int) -> int | None:
+        signal_ranges: List[Tuple[int, int]] = list()
+
+        for sensor in self.sensors:
+            distance_sensor_to_beacon = sensor.get_distance()
+            distance_sensor_to_y = abs(sensor.position_y - y)
+            signal_for_y = distance_sensor_to_beacon - distance_sensor_to_y
+            if signal_for_y >= 0:
+                signal_ranges.append((sensor.position_x - signal_for_y, sensor.position_x + signal_for_y))
+
+        signal_ranges.sort(key=lambda r: r[0] * 10000000 + r[1])
+
+        check_x = 0
+        for signal_range in signal_ranges:
+            if check_x >= signal_range[0]:
+                check_x = max(check_x, signal_range[1] + 1)
+            else:
+                return check_x
+
+        if check_x < limit_x:
+            return check_x
+        else:
+            return None
 
 
 def parse_input_file() -> Field:
@@ -78,11 +102,24 @@ def parse_input_file() -> Field:
     return Field(lines)
 
 
-def level15(y: int) -> Tuple[int, int]:
+def level15(y_level_1: int, max_level_2: int) -> Tuple[int, int]:
     field = parse_input_file()
-    return field.calc_signal_spots_for_line(y), 0
+
+    level_1_line = field.calc_signal_spots_for_line(y_level_1)
+    level_1 = len(list(filter(lambda p: p == Position.Signal, level_1_line)))
+
+    for y in range(0, max_level_2 + 1):
+        if y > 0 and y % 10000 == 0:  # prevent for tests
+            print(f"Checking line {y}", flush=True)
+        uncovered_x = field.get_uncovered_x(y, max_level_2)
+        if uncovered_x is not None:
+            return level_1, uncovered_x * 4000000 + y
 
 
 if __name__ == "__main__":
-    _signal_spots, _num = level15(2000000)
-    print(f"Signal spots (1): {_signal_spots}")
+    startTime = time.time()
+    _signal_spots, _tuning_frequency = level15(2000000, 4000000)
+    print(f"Signal spots: {_signal_spots}")
+    print(f"Tuning frequency: {_tuning_frequency}")
+    executionTime = time.time() - startTime
+    print("Execution time in seconds: " + str(executionTime))
--- tests/test_level15.py
+++ tests/test_level15.py
@@ -2,5 +2,6 @@ from level15 import level15
 
 
 def test_level15():
-    _signal_spots, _num = level15(10)
+    _signal_spots, _distress_beacon_frequency = level15(10, 20)
     assert _signal_spots == 26
+    assert _distress_beacon_frequency == 56000011
