5accddadb1a05ecb73b713ec1b0c3cfbf27170b0
Dominik "Dodo" Dopplinger <dom.dopplinger@gmail.com>
2022-12-24 23:37:05 +0100

    Solution level 19

--- solutions/level19.py
+++ solutions/level19.py
@@ -10,6 +10,7 @@ OBSIDIAN = "obsidian"
 GEODE = "geode"
 
 ALL_MATERIALS = [ORE, CLAY, OBSIDIAN, GEODE]
+ALL_BUT_GEODE = [ORE, CLAY, OBSIDIAN]
 
 
 class Materials:
@@ -18,7 +19,8 @@ class Materials:
     def __init__(self, materials: Dict[str, int]):
         self.items = materials.copy()
         for material in ALL_MATERIALS:
-            if material not in self.items: self.items[material] = 0
+            if material not in self.items:
+                self.items[material] = 0
 
     def is_contained_in(self, other):
         for material in ALL_MATERIALS:
@@ -32,20 +34,6 @@ class Materials:
 
         for material in ALL_MATERIALS:
             self.items[material] -= other.items[material]
-
-    def add(self, other):
-        for material in ALL_MATERIALS:
-            self.items[material] += other.items[material]
-
-    def has_any(self, materials: List[str]) -> bool:
-        for material in materials:
-            if self.items[material] > 0:
-                return True
-        return False
-
-    def get_max(self) -> Tuple[List[str], int]:
-        max_material = max(self.items.values())
-        return [material for material in self.items if self.items[material] == max_material], max_material
 
 
 class Blueprint:
@@ -63,8 +51,9 @@ class Blueprint:
             GEODE: Materials({ORE: int(parts[27]), OBSIDIAN: int(parts[30])}),
         }
         self.max_robots_needed = {}
-        for material in ALL_MATERIALS:
-            self.max_robots_needed[material] =
+        for material in ALL_BUT_GEODE:
+            self.max_robots_needed[material] = max(map(lambda cost: cost.items[material], self.robot_cost.values()))
+        self.max_robots_needed[GEODE] = 1000
 
     def can_build(self, robot: str, materials: Materials) -> bool:
         return self.robot_cost[robot].is_contained_in(materials)
@@ -78,130 +67,121 @@ def parse_input_file() -> List[Blueprint]:
     return list(map(Blueprint, lines))
 
 
-def strategy_prefer_later(blueprint: Blueprint, materials: Materials, _: Robots) -> str | None:
-    for robot_type in [GEODE, OBSIDIAN, CLAY, ORE]:
-        if blueprint.can_build(robot_type, materials):
-            return robot_type
-    return None
-
-
-def strategy_calc_resource_need(blueprint: Blueprint, materials: Materials, robots: Robots) -> str | None:
-    if blueprint.can_build(GEODE, materials):
-        return GEODE
-
-    needed_materials = Materials({
-        ORE: 0,
-        CLAY: 0,
-        OBSIDIAN: 0,
-        GEODE: 0,
-    })
-    needed_materials.add(blueprint.robot_cost[GEODE])
-    materials_served_by_robots = [material for material in ALL_MATERIALS if material in robots]
-
-    most_needed_materials, num_needed = needed_materials.get_max()
-    most_needed_materials.reverse()
-    for most_needed_material in most_needed_materials:
-        if blueprint.can_build(most_needed_material, materials):
-            return most_needed_material
-        needed_materials.add(blueprint.robot_cost[most_needed_material])
-
-
-def strategy_2_by_2(blueprint: Blueprint, materials: Materials, robots: Robots) -> str | None:
-    num_robots = sum(robots.values())
-    robot = ALL_MATERIALS[(num_robots // 2) % 4]
-    if blueprint.can_build(robot, materials):
-        return robot
-    return None
-
-
-def mine(blueprint: Blueprint, strategy) -> int:
-    materials = Materials({})
-    robots = {
-        ORE: 1,
-        CLAY: 0,
-        OBSIDIAN: 0,
-        GEODE: 0,
-    }
-    for _ in range(24):
-        new_robot = strategy(blueprint, materials, robots)
-        for material in ALL_MATERIALS:
-            materials.items[material] += robots[material]
-        if new_robot is not None:
-            blueprint.consume_resources(new_robot, materials)
-            robots[new_robot] += 1
-    return materials.items[GEODE]
-
-
-def calc_best_num_geodes(blueprint: Blueprint) -> int:
-    return max(
-        mine(blueprint, strategy_prefer_later),
-        mine(blueprint, strategy_2_by_2),
-    )
-
-
 class State:
     minute: int
     robots: Robots
     materials: Materials
+    target_robot: str
 
-    def __init__(self, minute: int, robots: Robots, materials: Materials):
+    def __init__(self, minute: int, robots: Robots, materials: Materials, target_robot: str):
         self.minute = minute
         self.robots = robots
         self.materials = materials
+        self.target_robot = target_robot
 
     def num_geodes(self) -> int:
         return self.materials.items[GEODE]
 
-    def should_build(self, blueprint: Blueprint, robot: str) -> bool:
-        return self.robots[robot] <
+    def can_build_any_new_bot(self, blueprint: Blueprint) -> bool:
+        for robot in self.robots:
+            if self.robots[robot] == 0 and blueprint.can_build(robot, self.materials):
+                return True
+        return False
+
+    def can_build_target(self, blueprint: Blueprint) -> bool:
+        return blueprint.can_build(self.target_robot, self.materials)
 
 
-def max_for_blueprint(blueprint: Blueprint):
-    states: List[List[State]] = [[]]
-    states[0].append(State(0, {ORE: 1, CLAY: 0, OBSIDIAN: 0, GEODE: 0}, Materials({})))
+def can_build_when_waiting(robots: Robots, robot: str):
+    if robot == ORE or robot == CLAY:
+        return True
+    elif robot == OBSIDIAN:
+        return robots[CLAY] > 0
+    elif robot == GEODE:
+        return robots[OBSIDIAN] > 0
+
+
+def should_build(blueprint: Blueprint, robots: Robots, robot: str) -> bool:
+    return robots[robot] < blueprint.max_robots_needed[robot]
+
+
+def get_max_geodes(states: List[State]) -> bool:
     max_geodes = 0
+    for state in states:
+        max_geodes = max(max_geodes, state.num_geodes())
+    return max_geodes
 
-    for minute in range(24):
+
+def max_for_blueprint(blueprint: Blueprint, minutes: int):
+    states: List[List[State]] = [[]]
+    states[0].append(State(0, {ORE: 1, CLAY: 0, OBSIDIAN: 0, GEODE: 0}, Materials({}), ORE))
+    states[0].append(State(0, {ORE: 1, CLAY: 0, OBSIDIAN: 0, GEODE: 0}, Materials({}), CLAY))
+    max_geode_bots = 0
+
+    for minute in range(minutes):
         states.append([])
         for state in states[minute]:
-            if state.num_geodes() >= max_geodes - 2:
-                new_state = create_new_state(blueprint, state, None)
-                states[minute + 1].append(new_state)
-                max_geodes = max(max_geodes, new_state.num_geodes())
-                for material in ALL_MATERIALS:
-                    if blueprint.can_build(material, state.materials) and state.should_build(blueprint, material):
-                        new_state = create_new_state(blueprint, state, material)
-                        states[minute + 1].append(new_state)
-                        max_geodes = max(max_geodes, new_state.num_geodes())
+            new_states = create_next_states(state, blueprint)
+            for new_state in new_states:
+                if new_state.robots[GEODE] >= max_geode_bots or new_state.target_robot == GEODE:
+                    max_geode_bots = max(max_geode_bots, new_state.robots[GEODE])
+                    states[minute + 1].append(new_state)
 
-    for state in states[24]:
-        max_geodes = max(max_geodes, state.num_geodes())
+    max_geodes = get_max_geodes(states[minutes])
 
     return max_geodes
 
 
-def create_new_state(blueprint: Blueprint, seed_state: State, action: str | None) -> State:
+def create_next_states(state: State, blueprint: Blueprint) -> List[State]:
+    if state.can_build_target(blueprint):
+        return build(blueprint, state)
+    else:
+        return [wait(state)]
+
+
+def wait(seed_state: State) -> State:
     materials = Materials(seed_state.materials.items)
     robots = seed_state.robots.copy()
 
     for material in ALL_MATERIALS:
         materials.items[material] += seed_state.robots[material]
-    if action is not None:
-        blueprint.consume_resources(action, materials)
-        robots[action] += 1
 
-    return State(seed_state.minute + 1, robots, materials)
+    return State(seed_state.minute + 1, robots, materials, seed_state.target_robot)
+
+
+def build(blueprint: Blueprint, seed_state: State) -> List[State]:
+    materials = Materials(seed_state.materials.items)
+    robots = seed_state.robots.copy()
+
+    for material in ALL_MATERIALS:
+        materials.items[material] += seed_state.robots[material]
+
+    blueprint.consume_resources(seed_state.target_robot, materials)
+    robots[seed_state.target_robot] += 1
+
+    new_states = []
+    for material in ALL_MATERIALS:
+        if can_build_when_waiting(robots, material) and should_build(blueprint, robots, material):
+            new_states.append(State(seed_state.minute + 1, robots, materials, material))
+    return new_states
+
+
+def level19_part(minutes: int, blueprints: List[Blueprint]) -> Tuple[int, int]:
+    sum_quality = 0
+    geode_product = 1
+    for blueprint in blueprints:
+        num_geodes = max_for_blueprint(blueprint, minutes)
+        sum_quality += blueprint.id * num_geodes
+        geode_product *= num_geodes
+    return sum_quality, geode_product
 
 
 def level19() -> Tuple[int, int]:
     blueprints = parse_input_file()
-    sum_quality = 0
-    for blueprint in blueprints:
-        num_geodes = max_for_blueprint(blueprint)
-        sum_quality += blueprint.id * num_geodes
-    return sum_quality, 0
+    return level19_part(24, blueprints)[0], level19_part(32, blueprints[0:3])[1]
 
 
 if __name__ == "__main__":
-    _sum_quality, _num = level19()
-    print(f"Quality sum: {_sum_quality}")
+    _sum_quality_1, _geode_product_2 = level19()
+    print(f"Quality sum (1): {_sum_quality_1}")
+    print(f"Geode product (2): {_geode_product_2}")
--- tests/test_level19.py
+++ tests/test_level19.py
@@ -2,5 +2,6 @@ from level19 import level19
 
 
 def test_level19():
-    _sum_quality, _num = level19()
-    assert _sum_quality == 33
+    _sum_quality_1, _geode_product_2 = level19()
+    assert _sum_quality_1 == 33
+    assert _geode_product_2 == 62 * 56
